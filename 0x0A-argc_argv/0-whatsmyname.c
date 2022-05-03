25c4c8459d96d0fa72c84644b2424e8de37a5e10
userlog404 <101346110+userlog404@users.noreply.github.com>
2022-05-03 16:58:47 +0000

    Create 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *main - print the name
+ *
+ *@argc: number of parameter
+ *
+ *@argv: value of parameter
+ *
+ *Return: 0
+ */
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+int i = 0;
+printf("%s\n", argv[i]);
+return (0);
+}
