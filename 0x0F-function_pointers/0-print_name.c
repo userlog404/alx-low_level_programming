66fdc3373a78efa53c164347efec23699b05d9d8
userlog404 <101346110+userlog404@users.noreply.github.com>
2022-05-17 10:00:51 +0000

    Pointing north, let's sail!

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+/**
+ * print_name - prints name
+ * @name: name input
+ * @f: function name
+ */
+void print_name(char *name, void (*f)(char *))
+{
+if (name && f)
+{
+f(name);
+}
+}
---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *array_iterator - iterate through the array
+ *@array: array
+ *@size: size of the array
+ *@action: a void function
+ *Return: void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+unsigned int x;
+if (array && size && action)
+{
+for (x = 0; x < size; x++)
+{
+(*action)(array[x]);
+}
+}
+}
